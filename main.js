1bdf9c0dc2ad10cd8fbb77c3a02a0f724dcb777d
SistemasPrepa19 <sistemasprepa19@gmail.com>
2025-05-26 09:57:31 -0600

    Fix typos in educational institution name and resolve merge conflicts in JavaScript file; adjust CSS for experience section layout

--- main.js
+++ main.js
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
 const spotlight = document.querySelector(".spotlight");
 document.addEventListener("mousemove", (e) => {
   spotlight.style.setProperty("--x", `${e.clientX}px`);
   spotlight.style.setProperty("--y", `${e.clientY}px`);
 });
-=======
-const spotlight = document.querySelector(".spotlight");
-document.addEventListener("mousemove", (e) => {
-  spotlight.style.setProperty("--x", `${e.clientX}px`);
-  spotlight.style.setProperty("--y", `${e.clientY}px`);
-});
->>>>>>> 243c1b98400f0b209190b695978bec40d44a644b
